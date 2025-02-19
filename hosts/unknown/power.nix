a9d699f14625f4542767584ead7604a16ee450b1
Glitch-guy0 <prajwalm9845@gmail.com>
2025-02-19 16:54:28 +0530

    feat: power saving config

--- hosts/unknown/power.nix
+++ hosts/unknown/power.nix
@@ -1,4 +1,19 @@
 {config, pkgs, ...}:
 {
-  
+  powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  # acpid for laptops
+  services.acpid.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq = {
+    enable = true;
+  };
+   # Define custom INI configuration for auto-cpufreq
+  environment.etc."auto-cpufreq/config.ini".text = ''
+    [settings]
+    enabled = true
+    min_freq = 800
+    max_freq = 4200
+    scaling_governor = performance
+  '';
 }
