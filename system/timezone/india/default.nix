e14e40c137dbfb575684f6e35d4eedbb4e6086f2
Glitch-guy0 <prajwalm9845@gmail.com>
2025-02-19 17:13:18 +0530

    fix: locale warnings

--- system/timezone/india/default.nix
+++ system/timezone/india/default.nix
@@ -3,17 +3,25 @@
   time.timeZone = "Asia/Kolkata";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ 
-    "en_IN/UTF-8" 
+    "en_US.UTF-8/UTF-8"
   ];
   i18n.extraLocaleSettings = {
+    LANGUAGE = "en_IN";
+    LC_ALL = "en_IN";
+    LC_CTYPE = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_COLLATE = "en_IN";
+    LC_TIME = "en_IN";
+    LC_MESSAGES = "en_IN";
+    LC_MONETARY = "en_IN";
     LC_ADDRESS = "en_IN";
     LC_IDENTIFICATION = "en_IN";
     LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
     LC_PAPER = "en_IN";
     LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
+    LC_NAME = "en_IN";
+    LANG = "en_US.UTF-8";
   };
+
+  environment.variables.LANG = "en_US.UTF-8";
 }
